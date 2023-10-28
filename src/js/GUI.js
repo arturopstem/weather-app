9bd23822440bbfdbd4473bf9d3ba3496e1a93f8b
arturop <67555134+arturopstem@users.noreply.github.com>
2023-10-28 06:25:35 -0500

    Add degree buttons functions

--- src/js/GUI.js
+++ src/js/GUI.js
@@ -6,4 +6,36 @@ function removeLoading(element) {
   element.removeAttribute('aria-busy');
 }
 
-export { showLoading, removeLoading };
+function colorDegreeBtn(degree) {
+  const degreeBtns = document.querySelectorAll('button[data-degree]');
+  degreeBtns.forEach((btn) => {
+    if (btn.dataset.degree === degree) {
+      btn.classList.remove('secondary', 'outline');
+      btn.classList.add('primary');
+    } else {
+      btn.classList.remove('primary');
+      btn.classList.add('secondary', 'outline');
+    }
+  });
+}
+
+function initDegreeBtns(degree = 'C') {
+  const storedDegree = localStorage.getItem('degree');
+  if (storedDegree) {
+    colorDegreeBtn(storedDegree);
+  } else {
+    localStorage.setItem('degree', degree);
+    colorDegreeBtn(degree);
+  }
+}
+
+function degreeBtnsEventHandler(e) {
+  if (e.target.nodeName === 'BUTTON') {
+    const button = e.target;
+    const { degree } = button.dataset;
+    localStorage.setItem('degree', degree);
+    colorDegreeBtn(degree);
+  }
+}
+
+export { showLoading, removeLoading, degreeBtnsEventHandler, initDegreeBtns };
